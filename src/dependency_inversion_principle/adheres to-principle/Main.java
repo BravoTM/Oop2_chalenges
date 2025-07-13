0a55fcf96b351b1f2617ebbead8f90a1764df97f
Alpha <bra81730@gmail.com>
2025-07-13 22:00:09 +0300

    final edit on dependancy invertion test

---
+++ src/dependency_inversion_principle/adheres to-principle/Main.java
@@ -0,0 +1,11 @@
+package dependency_inversion_principle.adheres_to_principle;
+
+public class Main {
+    public static void main(String[] args) {
+        Switchable bulb = new LightBulb();
+        Switch mySwitch = new Switch(bulb);
+        
+        mySwitch.operate(true);  // Output: LightBulb is ON
+        mySwitch.operate(false); // Output: LightBulb is OFF
+    }
+}
